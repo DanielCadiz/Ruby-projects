d8148907706abb0b6bd9b964fe5c133b4cf77910
DanielCadiz <stephencadiz014@gmail.com>
2021-05-05 18:53:54 +0800

    First Activity

--- age.rb
+++ age.rb
@@ -1,14 +1,15 @@
-puts "1.)"
+##Activity 1
+puts "Activity 1"
 nums = [1,2,3,4,5,6,7,8,9,10]
 
 nums.each do |num|
     puts num
 end
 
+##Activity 2
+puts "\n\nActivity 2"
 
-puts "\n\n2.)"
 h = {a:1, b:2, c:3, d:4}
-#puts h.each {|key|}
 h.each {|key| puts "#{key}"}
 
 puts "\nb:#{h[:b]}\n\n"
@@ -16,26 +17,17 @@ puts "\nb:#{h[:b]}\n\n"
 h[:e] = 5
 h.each {|key| puts "#{key}"}
 
+##Activity 3
+puts "\n\nActivity 3"
+contact_data = [["ana@email.com", "123 Main st.", "555-123-4567"],["avion@email.com", "404 Not Found Dr.", "123-234-3454"]]
+contacts = {"Analyn Cajocson" => {}, "Avion School" => {}}
 
-puts "\n\n3.)"
-info = [["1@email.com", "city1 street1", "123-123-1234"],["2@email.com", "city2 street2", "567-567-5678"]]
-puts "info 1:"
-puts info[0]
-puts "\ninfo 2:\n"
-puts info[1]
-puts
-person = {"person1" => {}, "person2" => {}}
-#person.each {|person| puts "#{person}"}
-person.each do |person|
-    puts person
-end
-puts
-person["person1"] = info[0]
-person["person2"] = info[1]
-puts "person1's info:\n#{person["person1"]}\n\n"
-puts "person2's info:\n#{person["person2"]}\n\n"
+contacts["Analyn Cajocson"] = contact_data[0]
+contacts["Avion School"] = contact_data[1]
+puts "#{contacts}\n\n"
 
-puts "4.)"
+##Activity 4
+puts "Activity 4"
 puts "How old are you?"
 age = gets.chomp.to_i
 
