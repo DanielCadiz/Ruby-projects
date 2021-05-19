4aae3194a64ac27eeb1019bb3340542bc86fb980
DanielCadiz <stephencadiz014@gmail.com>
2021-05-20 01:50:45 +0800

    First Activity

--- age.rb
+++ age.rb
@@ -22,9 +22,14 @@ puts "\n\nActivity 3"
 contact_data = [["ana@email.com", "123 Main st.", "555-123-4567"],["avion@email.com", "404 Not Found Dr.", "123-234-3454"]]
 contacts = {"Analyn Cajocson" => {}, "Avion School" => {}}
 
-contacts["Analyn Cajocson"] = contact_data[0]
-contacts["Avion School"] = contact_data[1]
-puts "#{contacts}\n\n"
+contacts['Analyn Cajocson'][:email] = contact_data[0][0]
+contacts['Analyn Cajocson'][:address] = contact_data[0][1]
+contacts['Analyn Cajocson'][:phone] = contact_data[0][2]
+contacts['Avion School'][:email] = contact_data[1][0]
+contacts['Avion School'][:address] = contact_data[1][1]
+contacts['Avion School'][:phone] = contact_data[1][2]
+
+puts contacts
 
 ##Activity 4
 puts "Activity 4"
