f090ca69009f569a5f77cd673c9a30c98778e97a
DanielCadiz <stephencadiz014@gmail.com>
2021-06-10 22:01:49 +0800

    Count of Positives, Sum of Negatives

--- positive_negative.rb
+++ positive_negative.rb
@@ -1,27 +1,28 @@
-input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
-#input = []
-pos_count = 0
-neg_sum = []
-answer = []
-
-if input != [] && input != nil
-    input.each do |num|
-        if num > 0
-            pos_count += 1
-        elsif num < 0
-            neg_sum << num
-        end
-    end
-    neg_sum.each do |neg|
-        if neg_sum[0] != neg
-            neg_sum[0] += neg
-        #elsif neg_sum == [] || neg_sum == nil
-            #neg_sum = [0]
-        end
-    end
-    answer << pos_count
-    answer << neg_sum[0]
-    puts "#{answer}"
-elsif input == [] || input == nil
-    puts input
-end
+def count_positives_sum_negatives(lst)
+    #your code here
+  pos_count = 0
+  neg_sum = []
+  answer = []
+  
+  if lst != [] && lst != nil
+      lst.each do |num|
+          if num > 0
+              pos_count += 1
+          elsif num < 0
+              neg_sum << num
+          end
+      end
+      neg_sum.each do |neg|
+          if neg_sum[0] != neg
+              neg_sum[0] += neg
+          end
+      end
+      answer << pos_count
+      answer << neg_sum[0]
+      puts "#{answer}"
+  elsif lst == [] || lst == nil
+      puts lst
+  end
+  end
+  input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
+  count_positives_sum_negatives(input)  
